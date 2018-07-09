ee663140ed607f625c2fb5931ebf536659c280f3
Elsterka <38614398+Elsterka@users.noreply.github.com>
2018-07-09 16:18:37 +0300

    Create cat_1.php

---
+++ cat_1.php
@@ -0,0 +1,26 @@
+<?php
+class Animal {
+ 
+    public $any_name;
+ 
+    public function __construct($name) {
+        $this->any_name = $name;
+    }
+ 
+    public function getName() {
+        return $this->any_name; 
+    }
+ 
+}
+ 
+class Cat extends Animal {
+    
+    public function meow() {
+        $catname = $this->getName();
+        return "Cat $catname is saying meow";
+    }
+ 
+}
+ 
+$cat = new Cat('Garfield');
+echo $cat->meow();
